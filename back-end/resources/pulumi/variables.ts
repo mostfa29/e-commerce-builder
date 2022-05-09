51840ac4d28284c836fde60f6d541195dce75a1a
mustapha-amin <madh@proosoft.com>
2022-05-09 12:03:58 +0100

    initialize saas project

--- back-end/resources/pulumi/variables.ts
+++
@@ -1,6 +0,0 @@
-import { Output } from '@pulumi/pulumi';
-
-export const variables = {
-  region: 'eu-west-1' as const, // <-- change this to your region
-  dynamoDBTables: {} as Record<string, Output<string>>,
-};
--- back-end/src/types/cognito-user.ts
+++
@@ -1,6 +0,0 @@
-export interface CognitoUser {
-  id: string;
-  username: string;
-  email: string;
-  createdAt: Date | string;
-}
